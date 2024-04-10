7ad14e05f9d434fd7e9c8a32fe48f824801c8401
AndreySmirnoffv <smirnoffa675@gmail.com>
2024-04-10 17:28:17 +0200

    new update

--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -2,9 +2,11 @@ const { startKeyboard } = require("../keyboard/keyboard")
 
 async function checkSub(bot, msg){
     try{
-        const chatMember = await bot.getChatMember('-1002056424069', msg.message.from.id);
+        const chatMember = await bot.getChatMember('@ai_midjourney017', msg.message.chat.id);
         if (chatMember && (chatMember.status === "member" || chatMember.status === "administrator" || chatMember.status === "creator")) {
             await bot.sendMessage(msg.message.chat.id, "Привет, я Безумная Нейросеть, и я готова помогать тебе абсолютно бесплатно. На сегодня я умею:\n-отвечать на любые текстовые запросы\n- генерировать фото по некоторым запросам.\nОсталось всего пару кликов.", startKeyboard)
+        }else{
+            await bot.sendMessage(msg.message.chat.id, "Вы не подписаны на канал вам надо подписаться чтобы пройти дальше")
         }
     }catch(error){
         console.log(error)
